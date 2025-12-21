8a929b25034e764135eba2dc810931b96a1c9371
egodfrey1618 <edward@hetzner>
2025-12-21 11:07:45 +0000

    Read a command and print an error

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io;
+use std::io::{Write};
+
+enum Command {
+    Command(String),
+}
+
+fn read_command() -> Result<Command, io::Error> {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s)?;
+
+    Ok(Command::Command(s))
+}
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush().expect("Error when flushing stdout");
+    let command = read_command().expect("IO error when reading command");
+
+    match command {
+        Command::Command(s) => print!("Unknown command: {s}"),
+    }
 }
